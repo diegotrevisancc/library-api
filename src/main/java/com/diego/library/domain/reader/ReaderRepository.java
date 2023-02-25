da041a6186c520730fd4d4a0976686ec54d74ea7
diegotrevisancc <diego.trevisan.cc@gmail.com>
2023-02-25 15:13:29 -0300

    created a repository interface to store reader objects in database

---
+++ src/main/java/com/diego/library/domain/reader/ReaderRepository.java
@@ -0,0 +1,6 @@
+package com.diego.library.domain.reader;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReaderRepository extends JpaRepository<Reader, Long> {
+}
