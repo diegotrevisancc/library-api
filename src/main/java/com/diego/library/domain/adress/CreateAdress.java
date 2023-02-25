63955e35b3f4a1f4a12e3064ccfa6864b3614704
diegotrevisancc <diego.trevisan.cc@gmail.com>
2023-02-25 17:08:40 -0300

    added bean validation

--- src/main/java/com/diego/library/domain/adress/CreateAdress.java
+++ src/main/java/com/diego/library/domain/adress/CreateAdress.java
@@ -1,6 +1,18 @@
 package com.diego.library.domain.adress;
 
-public record CreateAdress(String street, String neighborhood, String number, String state, String city) {
+import jakarta.validation.constraints.NotNull;
+
+public record CreateAdress(
+        @NotNull
+        String street,
+        @NotNull
+        String neighborhood,
+        @NotNull
+        String number,
+        @NotNull
+        String state,
+        @NotNull
+        String city) {
     public CreateAdress(Adress adress) {
         this(adress.getStreet(), adress.getNeighborhood(), adress.getNumber(), adress.getState(), adress.getCity());
     }
--- src/main/java/com/diego/library/domain/reader/CreateReader.java
+++ src/main/java/com/diego/library/domain/reader/CreateReader.java
@@ -1,8 +1,19 @@
 package com.diego.library.domain.reader;
 
 import com.diego.library.domain.adress.CreateAdress;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 
-public record CreateReader(String name, String email, String telephoneNumber, CreateAdress adress) {
+public record CreateReader(
+        @NotNull
+        String name,
+        @NotNull @Email
+        String email,
+        @NotNull
+        String telephoneNumber,
+        @NotNull @Valid
+        CreateAdress adress) {
     public CreateReader(Reader reader) {
         this(reader.getName(), reader.getEmail(), reader.getTelephoneNumber(), new CreateAdress(reader.getAdress()));
     }
