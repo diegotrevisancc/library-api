5cc6b709b0d95ecb2530f9619e1b8decaf458e69
diegotrevisancc <diego.trevisan.cc@gmail.com>
2023-02-26 12:49:16 -0300

    created a get method with paginator

--- src/main/java/com/diego/library/controller/BookController.java
+++ src/main/java/com/diego/library/controller/BookController.java
@@ -8,13 +8,13 @@ import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import java.net.URI;
+
 
 @RestController
 @RequestMapping("/books")
@@ -23,10 +23,17 @@ public class BookController {
     BookRepository repository;
     @PostMapping
     @Transactional
-    public ResponseEntity postBook(@RequestBody @Valid CreateBook bookData, UriComponentsBuilder uriBuilder) {
+    public ResponseEntity<BookDetails> postBook(@RequestBody @Valid CreateBook bookData, UriComponentsBuilder uriBuilder) {
         Book book = new Book(bookData);
         repository.save(book);
         URI location = uriBuilder.path("/book/{id}").buildAndExpand(book.getId()).toUri(); //http://localhost:8080/book/id
         return ResponseEntity.created(location).body(new BookDetails(book));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<BookDetails>> getBook(@PageableDefault(size = 10, page=0,sort = {"title"}) Pageable paginator) {
+        System.out.println(paginator.toString());
+        Page<BookDetails> page = (Page<BookDetails>) repository.findAllByActiveTrue(paginator).map(BookDetails::new);
+        return ResponseEntity.ok(page);
     }
 }
--- src/main/java/com/diego/library/domain/book/BookRepository.java
+++ src/main/java/com/diego/library/domain/book/BookRepository.java
@@ -1,6 +1,10 @@
 package com.diego.library.domain.book;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Pageable;
+
 
 public interface BookRepository extends JpaRepository<Book, Long> {
+    Page<Book> findAllByActiveTrue(Pageable paginator);
 }
